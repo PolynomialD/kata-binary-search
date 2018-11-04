9e92dee5991772bf3025610dd0bcb89e2af74109
PolynomialD <steven.grindey@gmail.com>
2018-11-04 13:59:36 +0000

    passing more tests

--- src/BinarySearch.js
+++ src/BinarySearch.js
@@ -1,7 +1,23 @@
 class BinarySearch {
-  result () {
-    return -1
+  result (target, array) {
+    let startPos = Math.floor(array.length/2)
+    for (let i=0; i < array.length; i++) {
+      if (array[startPos] < target) {
+        startPos = Math.floor(startPos + ((array.length - startPos)/2))
+        console.log(startPos)
+      }
+      if (array[startPos] > target) {
+        startPos = Math.floor(startPos/2)
+        console.log(startPos)
+      }
+      console.log(startPos)
+      if (array[startPos] === target) var index = startPos
+    }
+    if (index) {
+      return index
+    } else {
+        return -1
+      }
   }
 }
-
-module.exports = BinarySearch
+      module.exports = BinarySearch
--- test/BinarySearchSpec.js
+++ test/BinarySearchSpec.js
@@ -5,4 +5,20 @@ describe('BinarySearch', () => {
     const binarySearch = new BinarySearch()
     binarySearch.result(5,[]).should.eql(-1)
   })
+  verify.it('should return the correct index of a target integer', () => {
+    const binarySearch = new BinarySearch()
+    binarySearch.result(5,[1,5,7]).should.eql(1)
+  })
+  verify.it('should return the correct index of a target integer', () => {
+    const binarySearch = new BinarySearch()
+    binarySearch.result(9,[1,2,3,4,5,6,7,8,9]).should.eql(8)
+  })
+  verify.it('should return the correct index of a target integer', () => {
+    const binarySearch = new BinarySearch()
+    binarySearch.result(3,[1,3,5,7,8,9,10,13,18,21,27,33,54,67]).should.eql(1)
+  })
+  verify.it('should return the correct index of a target integer', () => {
+    const binarySearch = new BinarySearch()
+    binarySearch.result(93,[3,9,22,34,42,54,66,70,75,81,85,88,90,91,92,93,97,101]).should.eql(15)
+  })
 })
