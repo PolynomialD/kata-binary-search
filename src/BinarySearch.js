6aa344a39a2fabfa2b25bec1bac36cda681a1067
PolynomialD <steven.grindey@gmail.com>
2018-11-09 12:53:18 +0000

    changed floor to round

--- src/BinarySearch.js
+++ src/BinarySearch.js
@@ -1,17 +1,17 @@
 class BinarySearch {
   result (target, array) {
-    let startPos = Math.floor(array.length/2)
-    let movement = Math.floor(startPos/2)
+    let startPos = Math.round(array.length/2)
+    let movement = Math.round(startPos/2)
     let newStartPos = startPos + movement
     
     for (let i=0; i < Math.log2(array.length); i++) {
       if (array[startPos] < target) {
-        newStartPos = Math.floor(startPos + movement)
+        newStartPos = Math.round(startPos + movement)
       }
       if (array[startPos] > target) {
-        newStartPos = Math.floor(startPos - movement)
+        newStartPos = Math.round(startPos - movement)
       }
-      movement = Math.abs(Math.floor((startPos - newStartPos)/2))
+      movement = Math.abs(Math.round((startPos - newStartPos)/2))
       startPos = newStartPos
       if (array[startPos] === target) return startPos
     }
